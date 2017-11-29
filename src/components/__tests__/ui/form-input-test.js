0840d8ab3626d64a567af54c9419de474483af03
Kumaresan <kumar@elix.ai>
2017-11-29 11:43:27 +0530

    Unit tests for components

--- src/components/__tests__/ui/form-input-test.js
+++ src/components/__tests__/ui/form-input-test.js
@@ -7,10 +7,20 @@ import React from 'react';
 import renderer from 'react-test-renderer';
 
 import FormInput from '@ui/FormInput';
+import FormInputSimple from '@ui/FormInputSimple';
 
 it('FormInput renders correctly', () => {
   const tree = renderer.create(
     <FormInput value={'John Smith'} />,
+  ).toJSON();
+
+  expect(tree).toMatchSnapshot();
+});
+
+
+it('FormInputSimple renders correctly', () => {
+  const tree = renderer.create(
+    <FormInputSimple value={'John Smith'} />,
   ).toJSON();
 
   expect(tree).toMatchSnapshot();
--- src/components/__tests__/ui/form-label-test.js
+++ src/components/__tests__/ui/form-label-test.js
@@ -7,10 +7,19 @@ import React from 'react';
 import renderer from 'react-test-renderer';
 
 import FormLabel from '@ui/FormLabel';
+import FormLabelSimple from '@ui/FormLabelSimple';
 
 it('FormLabel renders correctly', () => {
   const tree = renderer.create(
     <FormLabel>John Smith</FormLabel>,
+  ).toJSON();
+
+  expect(tree).toMatchSnapshot();
+});
+
+it('FormLabel renders correctly', () => {
+  const tree = renderer.create(
+    <FormLabelSimple>John Smith</FormLabelSimple>,
   ).toJSON();
 
   expect(tree).toMatchSnapshot();
