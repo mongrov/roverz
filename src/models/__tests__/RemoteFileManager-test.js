05de83aee97360c248fe407530fe62401b31fccc
ezhila <ezhil@elix.ai>
2018-01-30 17:25:19 +0530

    refractor remote file manager

--- src/models/__tests__/RemoteFileManager-test.js
+++ src/models/__tests__/RemoteFileManager-test.js
@@ -11,8 +11,9 @@ it('addAll', () => {
   expect(db.remotefiles.list).toHaveLength(0);
   db.remotefiles.addAll({});
   expect(db.remotefiles.list).toHaveLength(0);
+  expect(db.remotefiles.cacheList).toMatchObject({});
   db.remotefiles.deleteAll();
-  const objs = {
+  let objs = {
     1: 'https://mongrov.redflock.net/group/idc',
     abc: 'https://mongrov.redflock.net',
     xyz: 'https://www.google.com',
@@ -21,7 +22,39 @@ it('addAll', () => {
   };
   db.remotefiles.addAll(objs);
   expect(db.remotefiles.list).toHaveLength(5);
-  db.remotefiles.findById(null);
-  db.remotefiles.findById('xyz');
+
+  // null value will be replaced by empty string
+  objs = {
+    1: 'https://mongrov.redflock.net/group/idc',
+    abc: 'https://mongrov.redflock.net',
+    xyz: 'https://www.google.com',
+    4: 'https://www.gmail.com',
+    5: '',
+  };
+  expect(db.remotefiles.cacheList).toMatchObject(objs);
+  expect(db.remotefiles.findById('xyz')).not.toBeNull();
+  // try to search id which is not present
+  expect(db.remotefiles.findById(null)).toBeNull();
+  expect(db.remotefiles.findById('lmn')).toBeNull();
   db.remotefiles.deleteAll();
+});
+
+it('cacheList', () => {
+  var db = new Database();
+  db.switchDb('mongrov', 'test');
+  // lets reset the db
+  db.reset();
+  // initial it should be empty
+  expect(db.remotefiles.cacheList).toMatchObject({});
+
+  // after populated cachelist should have the same
+  const objs = {
+    1: 'https://mongrov.redflock.net/group/idc',
+    abc: 'https://mongrov.redflock.net',
+    xyz: 'https://www.google.com',
+    4: 'https://www.gmail.com',
+    5: '',
+  };
+  db.remotefiles.addAll(objs);
+  expect(db.remotefiles.cacheList).toMatchObject(objs);
 });
